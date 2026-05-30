ee69a84ab2f13198ce6c98e5e4ebfe6f28b6e2be
its-kaktos <arman.hmpr@gmail.com>
2026-05-30 17:56:05 +0330

    nvim: Add ruff and basedpyright for lsp of python and update Mason packages

--- nvim/.config/nvim/lua/custom/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp-config.lua
@@ -185,7 +185,8 @@ return {
         },
         jsonls = {},
         -- clangd = {},
-        -- pyright = {},
+        basedpyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
