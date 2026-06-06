9c74af3d73e84e4ddec9cf188a80e25eeb2b2d6f
its-kaktos <arman.hmpr@gmail.com>
2026-06-06 09:13:54 +0330

    nvim: Uptade packages and add refactoring.lua

---
+++ nvim/.config/nvim/lua/custom/plugins/refactoring.lua
@@ -0,0 +1,12 @@
+return {
+  'ThePrimeagen/refactoring.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'lewis6991/async.nvim',
+  },
+  lazy = false,
+  config = function(_, opts)
+    require('refactoring').setup(opts)
+  end,
+}
